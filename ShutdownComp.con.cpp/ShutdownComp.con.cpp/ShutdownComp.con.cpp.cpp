0a2a2659daaa0577edcfaf721fc1118ca7dc8f36
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2019-12-18 15:51:07 -0500

    Add files via upload
    Method to shutdown Windows computer using cpp

---
+++ ShutdownComp.con.cpp/ShutdownComp.con.cpp/ShutdownComp.con.cpp.cpp
@@ -0,0 +1,9 @@
+// ShutdownComp.con.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	system("C:\\WINDOWS\\System32\\shutdown /s");
+}
